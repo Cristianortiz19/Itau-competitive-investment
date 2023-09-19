50fdd622a7c7164ed92e917b508b9026dc8e121a
Cristian Ortiz <cristclar012@gmai.com>
2023-09-18 20:48:09 -0500

    top bar component

---
+++ components/top-bar/top-bar.js
@@ -0,0 +1,49 @@
+class TopBar extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    static get observedAttributes() {
+        return []; 
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    attributeChangedCallback(propName, oldValue, newValue) {
+        this[propName] = newValue
+        this.render;
+    }
+
+    render() {
+        this.innerHTML = `
+        <link rel="stylesheet" href="./components/top-bar/style.css">
+        <ul class="top-bar">
+            <li>
+                <a href="">
+                    <img src="../../public/icons/top-bar-people.svg" alt="">
+                    <p>Personas</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../../public/icons/top-bar-business.svg" alt="">
+                    <p>Empresas</p>
+                </a>
+            </li>
+            <li>
+                <a href="" class="investment-option">
+                    <img src="../../public/icons/top-bar-investment.svg" alt="">
+                    
+                    <p style="color: #EC7404;"">Inversión</p>
+                </a>
+            </li>
+        </ul>
+        `;
+        console.log('aaaa')
+    }
+}
+
+customElements.define('top-bar', TopBar)
+export default TopBar;
